05bc00d9490fc264c823940156ba6be42e0ed6c3
LeoGoncha95 <99141299+LeoGoncha95@users.noreply.github.com>
2022-02-15 19:05:46 -0600

    Add: Files takas - Onbording

---
+++ Modulo_1/program.py
@@ -0,0 +1,9 @@
+#program.py
+
+#sum = 1 + 2
+#print(sum)
+
+
+print("Hola desde consola con dos comillas")    #comillas
+print('Hola desde consola con una comilla')     #Comilla simple
+#print(Hola desde consola)                     #Pruebas de funcionalidad
